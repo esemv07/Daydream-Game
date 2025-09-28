80926e0509165d799ad31fa858200aba30495b68
esemv07 <ruby.semaganda@gmail.com>
2025-09-28 13:56:58 +1000

    Add ghost texture

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,11 @@ extends Node
 var player_current_attack = false
 var player_health = 100
 
+var sword = false
+var spear = false
+var bow = false
+var sword2 = false
+var hammer = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
